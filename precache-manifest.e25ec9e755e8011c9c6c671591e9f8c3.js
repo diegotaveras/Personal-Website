323a032dec02b1bb6e3adb1880fc2a1b3baaeebc
diegotaveras <diegoat2@illinois.edu>
2023-02-14 11:09:26 -0600

    Updates

---
+++ precache-manifest.e25ec9e755e8011c9c6c671591e9f8c3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "1038185ecd8c0728324e",
+    "url": "/my-site/static/css/main.4925cde4.chunk.css"
+  },
+  {
+    "revision": "1038185ecd8c0728324e",
+    "url": "/my-site/static/js/main.1038185e.chunk.js"
+  },
+  {
+    "revision": "f881440d46e811047be5",
+    "url": "/my-site/static/js/1.f881440d.chunk.js"
+  },
+  {
+    "revision": "7ee4a7fa4bcc3607130e",
+    "url": "/my-site/static/js/2.7ee4a7fa.chunk.js"
+  },
+  {
+    "revision": "3a9f38473396864448ea",
+    "url": "/my-site/static/js/runtime~main.3a9f3847.js"
+  },
+  {
+    "revision": "a08f59f44e32ab5f5fe579b7b4088b97",
+    "url": "/my-site/static/media/linked-in-logo.a08f59f4.png"
+  },
+  {
+    "revision": "8b1d4bba2b7fd0ac7ec8ba2117df089d",
+    "url": "/my-site/static/media/mail-logo.8b1d4bba.png"
+  },
+  {
+    "revision": "171fb2af5e9e334ac50c46f37879b863",
+    "url": "/my-site/static/media/diego.171fb2af.JPG"
+  },
+  {
+    "revision": "94af267c2c39a3c37f27d806bf9ec04e",
+    "url": "/my-site/static/media/Diego-Taveras-Resume.94af267c.pdf"
+  },
+  {
+    "revision": "41257021b8d03a63e2272ef9057a4a91",
+    "url": "/my-site/index.html"
+  }
+];
