6473762a345fcfd6faa8082f111dd8b819039852
diegotaveras <diegoat2@illinois.edu>
2023-02-14 15:31:37 -0600

    switched to hashrouter to fix refresh issue

--- frontend/my-site/src/App.js
+++ frontend/my-site/src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 import NavBar from './NavBar';
 import React from 'react';
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes} from "react-router-dom"
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
 import Home from './pages/Home';
