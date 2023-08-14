069e3b5efa747f3d9bb5f7504ddd79f5381fb35a
diegotaveras <diegoat2@illinois.edu>
2023-08-14 01:06:46 -0400

    added radar chart ;changed designs

--- frontend/my-site/src/PlaylistInfo.js
+++ frontend/my-site/src/PlaylistInfo.js
@@ -7,6 +7,9 @@ import {useState, useEffect} from "react";
 import "./PlaylistInfo.css"
 import PopUp from './PopUp';
 
+import Radar from 'react-d3-radar';
+
+
 export default function PlaylistInfo() {
     const [playlistInfo, setPlaylistInfo] = useState();
     const [averageLoud, setAverageLoud] = useState(0.0);
@@ -42,7 +45,6 @@ export default function PlaylistInfo() {
     };
   }, []);
 
-  const [showPopUp, setShowPopUp] = useState(false);
 
   const [openedPopUpIndex, setOpenedPopUpIndex] = useState(-1); // Initialize to -1 (no pop-up open)
 
@@ -115,108 +117,157 @@ const [averages, setAverages] = useState({
       <div className="playlist-averages">
         <h1> Playlist Averages:</h1>
 
-        <h2 className="">
-        <PopUp
-          showPopUp={openedPopUpIndex === 0}
-          togglePopUp={() => togglePopUp(0)}
-          coords={mousePos}
-          desc="The overall loudness of a track in decibels (dB). Loudness values are averaged across the entire track and are useful for comparing relative loudness of tracks.  Values typical range between -60 and 0 db."
-        >
-          Loudness: {averages["loudness"].toFixed(2)}
-        </PopUp>
-        </h2>
+          <Radar
+  width={1000}
+  height={1000}
+  padding={0}
+  domainMax={1}
+  highlighted={null}
+  
+  onHover={(point) => {
+    if (point) {
+      console.log('hovered over a data point');
+    } else {
+      console.log('not over anything');
+    }
+  }}
+  data={{
+    
+    variables: [
+      // {key: 'loudness', label: 'Loudness'},
+      {key: 'danceability', label: 'Danceability'},
+      {key: 'energy', label: 'Energy'},
+      {key: 'valence', label: 'Valence'},
+      {key: 'instrumentalness', label: 'Instrumentalness'},
+      {key: 'tempo', label: 'Tempo'},
+      {key: 'liveness', label: 'Liveness'},
+      {key: 'acousticness', label: 'Acousticness'},
+      {key: 'speechiness', label: 'Speechiness'},
 
-        <h2>
-          <PopUp
-            showPopUp={openedPopUpIndex === 1}
-            togglePopUp={() => togglePopUp(1)}
-            coords={mousePos}
-            desc="A confidence measure from 0.0 to 1.0 of whether the track is acoustic. 1.0 represents high confidence the track is acoustic."
-          >
-            Acousticness: {averages["acousticness"].toFixed(2)}
-          </PopUp>
-        </h2>
-        <h2>
-          <PopUp
-            showPopUp={openedPopUpIndex === 2}
-            togglePopUp={() => togglePopUp(2)}
-            coords={mousePos}
-            desc="Danceability describes how suitable a track is for dancing based on a combination of musical elements including tempo, rhythm stability, beat strength, and overall regularity. A value of 0.0 is least danceable and 1.0 is most danceable."
-          >
-            Danceability: {averages["danceability"].toFixed(2)}
-          </PopUp>
-        </h2>
-        <h2>
-          <PopUp
-            showPopUp={openedPopUpIndex === 3}
-            togglePopUp={() => togglePopUp(3)}
-            coords={mousePos}
-            desc="Energy is a measure from 0.0 to 1.0 and represents a perceptual measure of intensity and activity. Typically, energetic tracks feel fast, loud, and noisy."
-          >
-            Energy: {averages["energy"].toFixed(2)}
-          </PopUp>
-        </h2>
-        <h2>
-          <PopUp
-            showPopUp={openedPopUpIndex === 4}
-            togglePopUp={() => togglePopUp(4)}
-            coords={mousePos}
-            desc="A measure from 0.0 to 1.0 describing the musical positiveness conveyed by a track. Tracks with high valence sound more positive (e.g. happy, cheerful, euphoric), while tracks with low valence sound more negative (e.g. sad, depressed, angry)."
-          >
-            Valence: {averages["valence"].toFixed(2)}
-          </PopUp>
-        </h2>
-        <h2>
-          <PopUp
-            showPopUp={openedPopUpIndex === 5}
-            togglePopUp={() => togglePopUp(5)}
-            coords={mousePos}
-            desc="The overall estimated tempo of a track in beats per minute (BPM). In musical terminology, tempo is the speed or pace of a given piece and derives directly from the average beat duration."
-          >
-            Tempo: {averages["tempo"].toFixed(2)}
-          </PopUp>
-        </h2>
-        <h2>
-          <PopUp
-            showPopUp={openedPopUpIndex === 6}
-            togglePopUp={() => togglePopUp(6)}
-            coords={mousePos}
-            desc="Predicts whether a track contains no vocals. The closer the instrumentalness value is to 1.0, the greater likelihood the track contains no vocal content."
-          >
-            Instrumentalness: {averages["instrumentalness"].toFixed(2)}
-          </PopUp>
-        </h2>
-        <h2>
-          <PopUp
-            showPopUp={openedPopUpIndex === 7}
-            togglePopUp={() => togglePopUp(7)}
-            coords={mousePos}
-            desc="Detects the presence of an audience in the recording. Higher liveness values represent an increased probability that the track was performed live."
-          >
-            Liveness: {averages["liveness"].toFixed(2)}
-          </PopUp>
-        </h2>
-        <h2>
-          <PopUp
-            showPopUp={openedPopUpIndex === 8}
-            togglePopUp={() => togglePopUp(8)}
-            coords={mousePos}
-            desc="A confidence measure from 0.0 to 1.0 of whether the track is acoustic. 1.0 represents high confidence the track is acoustic."
-          >
-            Acousticness: {averages["acousticness"].toFixed(2)}
-          </PopUp>
-        </h2>
-        <h2>
-          <PopUp
-            showPopUp={openedPopUpIndex === 9}
-            togglePopUp={() => togglePopUp(9)}
-            coords={mousePos}
-            desc="Speechiness detects the presence of spoken words in a track. The more exclusively speech-like the recording (e.g. talk show, audio book, poetry), the closer to 1.0 the attribute value."
-          >
-            Speechiness: {averages["speechiness"].toFixed(2)}
-          </PopUp>
-        </h2>
+    ],
+    sets: [
+      {
+        key: 'me',
+        label: 'My Scores',
+        values: {
+          // loudness: averages["loudness"].toFixed(2),
+          danceability: averages["danceability"].toFixed(2),
+          energy: averages["energy"].toFixed(2),
+          valence: averages["valence"].toFixed(2),
+          // tempo: averages["tempo"].toFixed(2),
+          liveness: averages["liveness"].toFixed(2),
+          acousticness: averages["acousticness"].toFixed(2),
+          speechiness: averages["speechiness"].toFixed(2)
+        },
+      }
+    ],
+  }}
+/>
+        <ul>
 
+        
+          <li>
+          <PopUp
+            showPopUp={openedPopUpIndex === 0}
+            togglePopUp={() => togglePopUp(0)}
+            coords={mousePos}
+            desc="The overall loudness of a track in decibels (dB). Loudness values are averaged across the entire track and are useful for comparing relative loudness of tracks.  Values typical range between -60 and 0 db."
+          >
+            Loudness: {averages["loudness"].toFixed(2)}
+          </PopUp>
+          </li>
+
+          <li>
+            <PopUp
+              showPopUp={openedPopUpIndex === 1}
+              togglePopUp={() => togglePopUp(1)}
+              coords={mousePos}
+              desc="A confidence measure from 0.0 to 1.0 of whether the track is acoustic. 1.0 represents high confidence the track is acoustic."
+            >
+              Acousticness: {averages["acousticness"].toFixed(2)}
+            </PopUp>
+          </li>
+          <li>
+            <PopUp
+              showPopUp={openedPopUpIndex === 2}
+              togglePopUp={() => togglePopUp(2)}
+              coords={mousePos}
+              desc="Danceability describes how suitable a track is for dancing based on a combination of musical elements including tempo, rhythm stability, beat strength, and overall regularity. A value of 0.0 is least danceable and 1.0 is most danceable."
+            >
+              Danceability: {averages["danceability"].toFixed(2)}
+            </PopUp>
+          </li>
+          <li>
+            <PopUp
+              showPopUp={openedPopUpIndex === 3}
+              togglePopUp={() => togglePopUp(3)}
+              coords={mousePos}
+              desc="Energy is a measure from 0.0 to 1.0 and represents a perceptual measure of intensity and activity. Typically, energetic tracks feel fast, loud, and noisy."
+            >
+              Energy: {averages["energy"].toFixed(2)}
+            </PopUp>
+          </li>
+          <li>
+            <PopUp
+              showPopUp={openedPopUpIndex === 4}
+              togglePopUp={() => togglePopUp(4)}
+              coords={mousePos}
+              desc="A measure from 0.0 to 1.0 describing the musical positiveness conveyed by a track. Tracks with high valence sound more positive (e.g. happy, cheerful, euphoric), while tracks with low valence sound more negative (e.g. sad, depressed, angry)."
+            >
+              Valence: {averages["valence"].toFixed(2)}
+            </PopUp>
+          </li>
+          <li>
+            <PopUp
+              showPopUp={openedPopUpIndex === 5}
+              togglePopUp={() => togglePopUp(5)}
+              coords={mousePos}
+              desc="The overall estimated tempo of a track in beats per minute (BPM). In musical terminology, tempo is the speed or pace of a given piece and derives directly from the average beat duration."
+            >
+              Tempo: {averages["tempo"].toFixed(2)}
+            </PopUp>
+          </li>
+          <li>
+            <PopUp
+              showPopUp={openedPopUpIndex === 6}
+              togglePopUp={() => togglePopUp(6)}
+              coords={mousePos}
+              desc="Predicts whether a track contains no vocals. The closer the instrumentalness value is to 1.0, the greater likelihood the track contains no vocal content."
+            >
+              Instrumentalness: {averages["instrumentalness"].toFixed(2)}
+            </PopUp>
+          </li>
+          <li>
+            <PopUp
+              showPopUp={openedPopUpIndex === 7}
+              togglePopUp={() => togglePopUp(7)}
+              coords={mousePos}
+              desc="Detects the presence of an audience in the recording with a range from 0.0 to 1.0. Higher liveness values represent an increased probability that the track was performed live."
+            >
+              Liveness: {averages["liveness"].toFixed(2)}
+            </PopUp>
+          </li>
+          <li>
+            <PopUp
+              showPopUp={openedPopUpIndex === 8}
+              togglePopUp={() => togglePopUp(8)}
+              coords={mousePos}
+              desc="A confidence measure from 0.0 to 1.0 of whether the track is acoustic. 1.0 represents high confidence the track is acoustic."
+            >
+              Acousticness: {averages["acousticness"].toFixed(2)}
+            </PopUp>
+          </li>
+          <li>
+            <PopUp
+              showPopUp={openedPopUpIndex === 9}
+              togglePopUp={() => togglePopUp(9)}
+              coords={mousePos}
+              desc="Speechiness detects the presence of spoken words in a track. The more exclusively speech-like the recording (e.g. talk show, audio book, poetry), the closer to 1.0 the attribute value."
+            >
+              Speechiness: {averages["speechiness"].toFixed(2)}
+            </PopUp>
+          </li>
+        </ul>
           
       </div>
   </div>
