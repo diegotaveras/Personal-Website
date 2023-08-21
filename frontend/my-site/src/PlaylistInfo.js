b5e8fb9e5e31ccbb26feaf9f0f48358d3743b604
diegotaveras <diegoat2@illinois.edu>
2023-08-21 01:00:38 -0500

    fixed small useEffect bug

--- frontend/my-site/src/PlaylistInfo.js
+++ frontend/my-site/src/PlaylistInfo.js
@@ -38,7 +38,7 @@ export default function PlaylistInfo() {
       fetch(`https://esefee855k.execute-api.us-east-1.amazonaws.com/Prod/api/user-playlists/${id}` + "/?user=" + localStorage.getItem("user_id"))
       .then(response => response.json())
       .then(data=> {
-        // console.log(data)
+        console.log(data)
         setPlaylistInfo(data)
     
     });
@@ -51,7 +51,6 @@ export default function PlaylistInfo() {
 
   const [openedPopUpIndex, setOpenedPopUpIndex] = useState(-1); // Initialize to -1 (no pop-up open)
 
-
   const togglePopUp = (index) => {
     setOpenedPopUpIndex((prevState) => (prevState === index ? -1 : index));
   };
@@ -62,7 +61,7 @@ export default function PlaylistInfo() {
       fetch(`https://esefee855k.execute-api.us-east-1.amazonaws.com/Prod/api/user-playlists/${id}/stats` + "/?user=" + localStorage.getItem("user_id"))
       .then(response => response.json())
       .then(data=> {
-        // console.log(data)
+        console.log(data)
         setStats(data)
           
       });
@@ -80,6 +79,7 @@ const [averages, setAverages] = useState({
     acousticness: 0.0,
     speechiness: 0.0,
   });
+  
   
   useEffect(() => {
     if (playlistInfo && playlistInfo.length > 0) {
@@ -106,13 +106,10 @@ const [averages, setAverages] = useState({
 
     
     setQualMap(qualObject);
-      
-      
-
     
     
   }
-}, [playlistInfo]);
+}, [playlistInfo, stats]);
 
 var data = {
   labels: ["Acousticness", "Danceability", "Energy", "Valence", "Instrumentalness", "Liveness", "Speechiness"],
@@ -155,7 +152,7 @@ const plugin = {
   
   return (
     <>
-    <div className='playlist-intro'> {}</div>
+    <div className='playlist-intro'> {console.log(playlistInfo)}</div>
 
     
     <div className="playlists-info">
@@ -195,7 +192,6 @@ const plugin = {
           <PopUp
             showPopUp={openedPopUpIndex === 0}
             togglePopUp={() => togglePopUp(0)}
-            
             desc="The overall loudness of a track in decibels (dB). Loudness values are averaged across the entire track and are useful for comparing relative loudness of tracks.  Values typical range between -60 and 0 db."
           >
             Loudness: {averages["loudness"].toFixed(2)}
@@ -206,7 +202,6 @@ const plugin = {
             <PopUp
               showPopUp={openedPopUpIndex === 1}
               togglePopUp={() => togglePopUp(1)}
-              
               desc="A confidence measure from 0.0 to 1.0 of whether the track is acoustic. 1.0 represents high confidence the track is acoustic."
             >
               Acousticness: {averages["acousticness"].toFixed(2)}
@@ -216,7 +211,6 @@ const plugin = {
             <PopUp
               showPopUp={openedPopUpIndex === 2}
               togglePopUp={() => togglePopUp(2)}
-              
               desc="Danceability describes how suitable a track is for dancing based on a combination of musical elements including tempo, rhythm stability, beat strength, and overall regularity. A value of 0.0 is least danceable and 1.0 is most danceable."
             >
               Danceability: {averages["danceability"].toFixed(2)}
@@ -226,7 +220,6 @@ const plugin = {
             <PopUp
               showPopUp={openedPopUpIndex === 3}
               togglePopUp={() => togglePopUp(3)}
-              
               desc="Energy is a measure from 0.0 to 1.0 and represents a perceptual measure of intensity and activity. Typically, energetic tracks feel fast, loud, and noisy."
             >
               Energy: {averages["energy"].toFixed(2)}
@@ -236,7 +229,6 @@ const plugin = {
             <PopUp
               showPopUp={openedPopUpIndex === 4}
               togglePopUp={() => togglePopUp(4)}
-              
               desc="A measure from 0.0 to 1.0 describing the musical positiveness conveyed by a track. Tracks with high valence sound more positive (e.g. happy, cheerful, euphoric), while tracks with low valence sound more negative (e.g. sad, depressed, angry)."
             >
               Valence: {averages["valence"].toFixed(2)}
@@ -246,7 +238,6 @@ const plugin = {
             <PopUp
               showPopUp={openedPopUpIndex === 5}
               togglePopUp={() => togglePopUp(5)}
-              
               desc="The overall estimated tempo of a track in beats per minute (BPM). In musical terminology, tempo is the speed or pace of a given piece and derives directly from the average beat duration."
             >
               Tempo: {averages["tempo"].toFixed(2)}
@@ -256,7 +247,6 @@ const plugin = {
             <PopUp
               showPopUp={openedPopUpIndex === 6}
               togglePopUp={() => togglePopUp(6)}
-              
               desc="Predicts whether a track contains no vocals. The closer the instrumentalness value is to 1.0, the greater likelihood the track contains no vocal content."
             >
               Instrumentalness: {averages["instrumentalness"].toFixed(2)}
@@ -266,7 +256,6 @@ const plugin = {
             <PopUp
               showPopUp={openedPopUpIndex === 7}
               togglePopUp={() => togglePopUp(7)}
-              
               desc="Detects the presence of an audience in the recording with a range from 0.0 to 1.0. Higher liveness values represent an increased probability that the track was performed live."
             >
               Liveness: {averages["liveness"].toFixed(2)}
@@ -276,7 +265,6 @@ const plugin = {
             <PopUp
               showPopUp={openedPopUpIndex === 9}
               togglePopUp={() => togglePopUp(9)}
-              
               desc="Speechiness detects the presence of spoken words in a track. The more exclusively speech-like the recording (e.g. talk show, audio book, poetry), the closer to 1.0 the attribute value."
             >
               Speechiness: {averages["speechiness"].toFixed(2)}
